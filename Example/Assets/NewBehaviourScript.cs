dd050762d237a409490f4c8c7ed0a5be0e13bafa
Kellenem <44127016+Kellenem@users.noreply.github.com>
2018-10-15 18:49:55 -0400

    Final Changes

---
+++ Example/Assets/NewBehaviourScript.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+public class TextMeshTimer : MonoBehaviour
+{
+
+    private TextMesh _textMesh;
+    private bool _isRunning;
+    private float _elapsedSeconds;
+
+    void Start()
+    {
+        _textMesh = GetComponent<TextMesh>();
+    }
+
+    void Update()
+    {
+        if (!_isRunning) return;
+
+        _elapsedSeconds += Time.deltaTime;
+        var timeSpan = TimeSpan.FromSeconds(_elapsedSeconds);
+        _textMesh.text = string.Format("{0:D2}:{1:D2}:{2:D2}", timeSpan.Hours, timeSpan.Minutes, timeSpan.Seconds);
+    }
+
+    public void StartTimer()
+    {
+        _isRunning = true;
+    }
+
+    public void ResetTimer()
+    {
+        _elapsedSeconds = 0;
+    }
+
+    public void StopTimer()
+    {
+        _isRunning = false;
+    }
+}
---
+++ Example/Assets/Winbox.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Winbox : MonoBehaviour {
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject.Find("Player").SendMessage("Finish");
+    }
+}
